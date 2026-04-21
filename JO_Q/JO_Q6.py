16bc5432b12a43301709124586042e30c5b38135
Ahri Kim <a_0810@naver.com>
2026-04-21 18:57:23 +0900

    2026.04.21

--- JO_Q/JO_Q6.py
+++ JO_Q/JO_Q6.py
@@ -64,7 +64,7 @@ while t != -1:
     else:
         l.pop()
 print(*l)
-'''
+
 #9390
 h = int(input())
 m = int(input())
@@ -77,3 +77,76 @@ elif h>10 and m < 10:
     print(f"{h}:0{m}")
 else:
     print(f"{h}:{m}")
+
+#9462
+n = int(input())
+i = list(map(int,input().split()))
+l = len(i)
+for k in range(l-1):
+    swapped = False
+    for j in range(0, l - k - 1):
+        if i[j] > i[j + 1]:
+            i[j], i[j + 1] = i[j + 1], i[j]
+            swapped = True
+    print(i)
+
+#1291
+s, e = map(int,input().split())
+while s<2 or s>9 or e<2 or e>9:
+    print("INPUT ERROR!")
+    s, e = map(int,input().split())
+if s < e :
+    for j in range(1, 10):
+        for i in range(s, e+1):
+            print(f"{i} * {j} = {i*j:2d}",end='   ')
+        print()
+else:
+    for j in range(1, 10):
+        for i in range(s,e-1,-1):
+            print(f"{i} * {j} = {i*j:2d}",end='   ')
+        print()
+
+#1341
+s, f = map(int,input().split())
+if s < f:
+    for j in range(s, f+1,1):
+        for i in range(1, 4, 1):
+            print(f"{j} * {i} = {i*j:2d}",end='   ')
+        print()
+        for i in range(4, 7, 1):
+            print(f"{j} * {i} = {i*j:2d}",end='   ')
+        print()
+        for i in range(7, 10, 1):
+            print(f"{j} * {i} = {i*j:2d}",end='   ')
+        print()
+        print()
+else:
+    for j in range(s, f-1, -1):
+        for i in range(1, 4, 1):
+            print(f"{j} * {i} = {i*j:2d}",end='   ')
+        print()
+        for i in range(4, 7, 1):
+            print(f"{j} * {i} = {i*j:2d}",end='   ')
+        print()
+        for i in range(7, 10, 1):
+            print(f"{j} * {i} = {i*j:2d}",end='   ')
+        print()
+        print()
+
+#1303
+n, m = map(int,input().split())
+a = n*m
+for i in range(1,n+1,1):
+    if i == 1:
+        for k in range(1,m+1,1):
+            print(k,end=' ')
+        print()
+    else:
+        for j in range((i-1)*m+1,i*m+1,1):
+            print(j,end=' ')
+        print()
+'''
+#1856
+
+
+
