e140891855faa06a9f735e5fff3f730b35bc3abe
Ahri Kim <a_0810@naver.com>
2026-04-28 18:58:01 +0900

    2026.04.28

--- JO_Q/JO_Q7.py
+++ JO_Q/JO_Q7.py
@@ -25,17 +25,41 @@ for z in range(n):
     for x in range(n):
         print(l[x][z],end=' ')
     print()
-'''
+
 #1338
 L=['A', 'B', 'C', 'D','E','F','G','H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
 n=int(input())
-l=[[0]*n for _ in range(n)]
-M = 2*n-1
-for i in range(0,n,1):
-    if i > 25:
-        i = i%26
-        J = 0
-    for j in range(n*(n+1)//2-1,0,-1):
-        l[i][J]=L[j]
-        J +=1
-    print(l)
+l=[[0]*t for t in range(1,n+1,1)]
+J = 0
+for i in range(n):
+    K=0
+    if i != 0:
+        J = i
+    for j in range(len(l[i])):
+        if J > 25:
+            J = J%26
+        l[i][K]=L[J]
+        J =J+n-j-1
+        K +=1
+for q in range(n):
+    print(f"{' '.join(l[q]):>{n*2-1}}")
+
+#3106
+A = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+N , B = map(int,input().split())
+l=[]
+r=1
+while N > 0:
+    r=N%B
+    N=N//B
+    l.append(A[r])
+print(''.join(l[::-1]))
+'''
+#1009
+n = 1
+while n!=0:
+    n = input()
+    s=0
+    for i in range(len(n)):
+        s+=int(n[i])
+    print(n[::-1], s)
