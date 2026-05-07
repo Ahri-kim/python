4a3cf17dc500b3e32e69b59980f7070fd91e3a93
Ahri Kim <a_0810@naver.com>
2026-05-07 18:52:54 +0900

    2026.05.07

--- PR_Q/PR_Q1.py
+++ PR_Q/PR_Q1.py
@@ -1,3 +1,4 @@
+'''
 import math
 
 def solution(numer1, denom1, numer2, denom2):
@@ -7,3 +8,27 @@ def solution(numer1, denom1, numer2, denom2):
     numer //= g
     denom //= g
     return [numer, denom]
+
+my_string="hi12392"
+
+L=list(range(0,10))
+def solution(my_string):
+    return sorted([int(i) for i in my_string if i.isdigit()])
+
+my_str='abc1Addfggg4556b'
+n=6
+
+def solution (my_str,n):
+    m=[]
+    while len(my_str)!=0:
+        m.append(my_str[:n])
+        my_str=my_str[n:]
+    return m
+'''
+def solution(numbers):
+    l = ["zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine"]
+    for i in l:
+        numbers = numbers.replace(i, str(l.index(i)))
+
+    answer = int(numbers)
+    return answer
--- SW_Q/SW_Q2.py
+++ SW_Q/SW_Q2.py
@@ -3,7 +3,7 @@
 n=input()
 a=int(input())
 print(f"{n}(은)는 {100-a+2019}년에 100세가 될 것입니다.")
-'''
+
 #4828
 t=int(input())
 for i in range(t):
@@ -11,3 +11,27 @@ for i in range(t):
     l=list(map(int,input().split()))
     l.sort()
     print(f"#{i+1} {l[-1]-l[0]}")
+'''
+#4880
+T=int(input())
+R=2, S=1, P=3
+R>S, R<P, S>P
+for i in range(T):
+    N=int(input())
+    l=list(map(int,input().split()))
+    mid=len(l)//2
+    while True:
+        for j in l[:mid-1]:
+            if l[j]>l[j+1]:
+                l[j+1] = 
+            else:
+                l.pop(l[j])
+        for k in range(mid,len(l)):
+            if l[k]>l[k+1]:
+                l.pop(k+1)
+            else:
+                l.pop(k)
+        if len(l)==2:
+
+            False
+            break
